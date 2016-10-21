99718e1496946f319cf5baf6e7e437a8e98b22e2
Raghav <raghavyadav258@gmail.com>
2016-10-21 13:46:48 -0500

    Initial commit

---
+++ IFC/src/IFCGeneration.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author raghavyadavm(raghavyadav258@gmail.com)
+ *
+ */
+public class IFCGeneration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
