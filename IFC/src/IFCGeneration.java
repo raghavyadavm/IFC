398b81c5ce2901fba79291e3f6792e120b8b6b67
raghavyadavm <raghavyadav258@gmail.com>
2016-10-23 15:13:32 -0500

    Apache POI testing

--- IFC/src/IFCGeneration.java
+++ IFC/src/IFCGeneration.java
@@ -1,3 +1,16 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
 /**
  * IFC generation Project
  */
@@ -7,12 +20,32 @@
  *
  */
 public class IFCGeneration {
+	
+	static XSSFRow row;
+
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws  IOException{
 		// TODO Auto-generated method stub
+		
+		FileInputStream fis = new FileInputStream(new File("D:\\test.xlsx"));
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet spreadSheet = workbook.getSheetAt(0);
+		Iterator<Row> rowIterator = spreadSheet.iterator();
+		while (rowIterator.hasNext()) {
+			row = (XSSFRow) rowIterator.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
+			while (cellIterator.hasNext()) {
+				Cell cell = (Cell) cellIterator.next();
+				System.out.println(cell.toString());								
+			}
+			
+		}
+		
+		workbook.close();
+		fis.close();
 
 	}
 
