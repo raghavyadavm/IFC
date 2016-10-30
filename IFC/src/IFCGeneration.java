eb633f766d0ccd3739194a0f138aec5a7517d450
raghavyadavm <raghavyadav258@gmail.com>
2016-10-30 18:11:17 -0500

    Align the new IFC Elements Index with the already generated IFC elements Index

--- IFC/src/IFCGeneration.java
+++ IFC/src/IFCGeneration.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -23,30 +27,35 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class IFCGeneration {
 	
-	static XSSFRow row;
-
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) throws  IOException{
-		// TODO Auto-generated method stub1
+	public IFCGeneration() throws IOException {
+		// TODO Auto-generated constructor stub
+	//int cc = getCount();
+	//System.out.println(cc);
 		
-		FileWriter fw = new FileWriter(new File("D:\\test.ifc"));
+		printExcel();
+	
+	}
+	
+	public void printExcel() throws IOException{
+		XSSFRow row;
+		FileWriter fw = new FileWriter(new File("D:\\test3.ifc"));
 		BufferedWriter bw = new BufferedWriter(fw);
 		
 		FileInputStream fis = new FileInputStream(new File("D:\\test2.xlsx"));
 		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		int counter = 0, cellCount = 0;
-		for (int i = 1; i <workbook.getNumberOfSheets(); i++) {
+		int cellCount = 0;
+		int counter = getCount();
+		
+		for (int i = 1; i <5; i++) {
 			
 			XSSFSheet spreadSheet = workbook.getSheetAt(i);
 			Iterator<Row> rowIterator = spreadSheet.iterator();
-			String zone = "#"+(++counter)+"= IFCPROPERTYSINGLEVALUE('"+spreadSheet.getSheetName()+"',$,IFCTEXT('0'),$);";
+			String zone = "#"+(++counter)+"= IFCPROPERTYSINGLEVALUE('Zone',$,IFCTEXT('"+spreadSheet.getSheetName()+"'),$);";
 			
 			System.out.println(zone);
 			bw.write(zone);
 			bw.newLine();
+			
 			if (rowIterator.hasNext()) {
 				
 				row = (XSSFRow) rowIterator.next();
@@ -55,8 +64,9 @@ public class IFCGeneration {
 				while (cellIterator.hasNext()) {
 					
 					Cell cell = (Cell) cellIterator.next();
-					System.out.println("#"+(++counter)+"= IFCPROPERTYSINGLEVALUE('"+cell.toString()+"',$,IFCTEXT('0'),$);");
-					String msg = "#"+counter+"= IFCPROPERTYSINGLEVALUE('"+cell.toString()+"',$,IFCTEXT('0'),$);";
+					//System.out.println(workbook.getSheetAt(i).getRow(1).getCell(cell.getColumnIndex()));
+					System.out.println("#"+(++counter)+"= IFCPROPERTYSINGLEVALUE('"+cell.toString()+"',$,IFCTEXT('"+(workbook.getSheetAt(i).getRow(1).getCell(cell.getColumnIndex())).getRawValue()+"'),$);");
+					String msg = "#"+counter+"= IFCPROPERTYSINGLEVALUE('"+cell.toString()+"',$,IFCTEXT('"+(workbook.getSheetAt(i).getRow(1).getCell(cell.getColumnIndex())).getRawValue()+"'),$);";
 					bw.write(msg);
 					bw.newLine();
 				}
@@ -78,6 +88,52 @@ public class IFCGeneration {
 		workbook.close();
 		fis.close();
 
+	}
+	
+	
+	public  int getCount() throws IOException{
+		FileReader fr = new FileReader(new File("D:\\test.ifc"));
+		BufferedReader br = new BufferedReader(fr);
+		
+		FileWriter fw = new FileWriter(new File("D:\\result1.ifc"));
+		BufferedWriter bw = new BufferedWriter(fw);
+		TreeSet<Integer> ts1 = new TreeSet<Integer>();
+		
+		
+		String line;
+		while((line = br.readLine()) != null)
+		{
+			
+			if (line.contains("#")) {
+				//System.out.println("true");
+			
+				String requiredString = line.substring(line.indexOf("#") + 1, line.indexOf("="));
+				//System.out.println(requiredString);
+				int num = Integer.parseInt(requiredString);
+				ts1.add(num);
+				bw.write(String.valueOf(num));
+				bw.newLine();
+			}	
+			
+		}
+		
+		//System.out.println(ts1);
+		System.out.println(ts1.last());
+		br.close();
+		bw.close();
+		return ts1.last();
+		
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws  IOException{
+		// TODO Auto-generated method stub1
+		
+		new IFCGeneration();
+		
 	}
 
 }
